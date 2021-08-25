805c7b224eb7e88e0bfb644496d13c7cedaed657
Leonardo-Antunes <leonardo.antunes@globalhitss.com.br>
2021-08-25 13:27:19 -0300

    vector icons changed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import {Image} from 'react-native'
+import React from 'react';
+import { Image } from 'react-native';
 
-import Logo from '../../assets/logo.png'
+import Logo from '../../assets/logo.png';
 
-import { Container } from './styles'
+import { Container } from './styles';
 
 const Header = () => {
   return (
     <Container>
       <Image source={Logo} />
     </Container>
-  )
-}
+  );
+};
 
 export default Header;
