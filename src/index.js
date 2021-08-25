2040927395883af0290788034fe99bfd605f4ede
Leonardo-Antunes <leonardo.antunes@globalhitss.com.br>
2021-08-25 13:04:12 -0300

    first commmit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { StatusBar } from 'react-native';
+
+import Routes from './routes';
+
+const App = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <Routes />
+  </>
+);
+
+export default App;
