f38172442c1dd81b7be96539c18118a6359a3672
Leonardo-Antunes <leonardo.antunes@globalhitss.com.br>
2021-08-26 06:34:43 -0300

    redux

--- src/pages/Catalog/index.js
+++ src/pages/Catalog/index.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
 import FloatingCart from '../../components/FloatingCart';
 
 import formatValue from '../../utils/formatValue';
+import api from '../../services/api';
 
 import {
   Container,
@@ -19,23 +20,18 @@ import {
   ProductTitle,
 } from './styles';
 
-export default function App() {
-  const [products, setProducts] = useState([
-    {
-      id: '1',
-      title: 'Assinatura Trimestral',
-      image_url:
-        'https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png',
-      price: 150,
-    },
-    {
-      id: '2',
-      title: 'Assinatura Trimestral',
-      image_url:
-        'https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png',
-      price: 150,
-    },
-  ]);
+export default function Catalog() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function loadProducts() {
+      const { data } = await api.get('/products');
+
+      setProducts(data);
+    }
+    loadProducts();
+  }, []);
+
   return (
     <Container>
       <ProductContainer>
